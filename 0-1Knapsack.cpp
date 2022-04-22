e9d99ae54b5b1b5ec6efa1a8fb79b0d93329c7dd
Vaibhavi <porevaibhavi5@gmail.com>
2022-04-22 22:50:29 +0530

    :sparkles: Changed Approach

--- 0-1Knapsack.cpp
+++ 0-1Knapsack.cpp
@@ -1,164 +1,85 @@
-// C++ program to solve knapsack problem using
-// branch and bound
-#include <bits/stdc++.h>
+//This ONE!!
+
+#include<bits/stdc++.h>
 using namespace std;
 
-// Structure for Item which store weight and corresponding
-// value of Item
-struct Item
-{
-	float weight;
-	int value;
-};
+vector<double> p;
+vector<double> w;
+vector<vector<double>> objList;
+int n, M;
 
-// Node structure to store information of decision
-// tree
-struct Node
-{
-	// level --> Level of node in decision tree (or index
-	//			 in arr[]
-	// profit --> Profit of nodes on path from root to this
-	//		 node (including this node)
-	// bound ---> Upper bound of maximum profit in subtree
-	//		 of this node/
-	int level, profit, bound;
-	float weight;
-};
-
-// Comparison function to sort Item according to
-// val/weight ratio
-bool cmp(Item a, Item b)
-{
-	double r1 = (double)a.value / a.weight;
-	double r2 = (double)b.value / b.weight;
-	return r1 > r2;
+void printSoln(vector<double> x){
+    int sum=0;
+    cout<<"\nItems in Knapsack - \n"<<endl;
+    cout<<"Object\tFraction"<<endl;
+    for(int i=0; i<x.size(); i++){
+        cout<<fixed<<setprecision(0)<<objList[i][3]<<"\t";
+        cout<<fixed<<setprecision(2)<<x[i]<<endl;
+    }
+    for(int i=0; i<n; i++){
+        sum+=(x[i])*(objList[i][1]);
+    }
+    cout<<"Maximum profit: "<<sum<<endl;
 }
 
-// Returns bound of profit in subtree rooted with u.
-// This function mainly uses Greedy solution to find
-// an upper bound on maximum profit.
-int bound(Node u, int n, int W, Item arr[])
-{
-	// if weight overcomes the knapsack capacity, return
-	// 0 as expected bound
-	if (u.weight >= W)
-		return 0;
-
-	// initialize bound on profit by current profit
-	int profit_bound = u.profit;
-
-	// start including items from index 1 more to current
-	// item index
-	int j = u.level + 1;
-	int totweight = u.weight;
-
-	// checking index condition and knapsack capacity
-	// condition
-	while ((j < n) && (totweight + arr[j].weight <= W))
-	{
-		totweight += arr[j].weight;
-		profit_bound += arr[j].value;
-		j++;
-	}
-
-	// If k is not n, include last item partially for
-	// upper bound on profit
-	if (j < n)
-		profit_bound += (W - totweight) * arr[j].value /
-										arr[j].weight;
-
-	return profit_bound;
+void Knapsack(){
+    vector<double> x(n, 0);
+    int i;
+    int remCapacity = M;
+    for(i=0; i<n; i++){
+        if(w[i]>remCapacity)
+            break;
+        x[i] = 1;
+        remCapacity-=objList[i][2];
+    }
+    printSoln(x);
 }
 
-// Returns maximum profit we can get with capacity W
-int knapsack(int W, Item arr[], int n)
-{
-	// sorting Item on basis of value per unit
-	// weight.
-	sort(arr, arr + n, cmp);
-
-	// make a queue for traversing the node
-	queue<Node> Q;
-	Node u, v;
-
-	// dummy node at starting
-	u.level = -1;
-	u.profit = u.weight = 0;
-	Q.push(u);
-
-	// One by one extract an item from decision tree
-	// compute profit of all children of extracted item
-	// and keep saving maxProfit
-	int maxProfit = 0;
-	while (!Q.empty())
-	{
-		// Dequeue a node
-		u = Q.front();
-		Q.pop();
-
-		// If it is starting node, assign level 0
-		if (u.level == -1)
-			v.level = 0;
-
-		// If there is nothing on next level
-		if (u.level == n-1)
-			continue;
-
-		// Else if not last node, then increment level,
-		// and compute profit of children nodes.
-		v.level = u.level + 1;
-
-		// Taking current level's item add current
-		// level's weight and value to node u's
-		// weight and value
-		v.weight = u.weight + arr[v.level].weight;
-		v.profit = u.profit + arr[v.level].value;
-
-		// If cumulated weight is less than W and
-		// profit is greater than previous profit,
-		// update maxprofit
-		if (v.weight <= W && v.profit > maxProfit)
-			maxProfit = v.profit;
-
-		// Get the upper bound on profit to decide
-		// whether to add v to Q or not.
-		v.bound = bound(v, n, W, arr);
-
-		// If bound value is greater than profit,
-		// then only push into queue for further
-		// consideration
-		if (v.bound > maxProfit)
-			Q.push(v);
-
-		// Do the same thing, but Without taking
-		// the item in knapsack
-		v.weight = u.weight;
-		v.profit = u.profit;
-		v.bound = bound(v, n, W, arr);
-		if (v.bound > maxProfit)
-			Q.push(v);
-	}
-
-	return maxProfit;
-}
-
-// driver program to test above function
-int main()
-{
-	int W = 10, i, n; // Weight of knapsack
-    cout<<"Enter the no. of objects"<<endl;
+int main(){
+    int val, i, j, temp;
+    cout<<"Enter maximum weight"<<endl;
+    cin>>M;
+    cout<<"Enter number of objects"<<endl;
     cin>>n;
-	Item arr[n];
-
-	for(i=0; i<n; i++){
-        cout<<"\nEnter Weight of Obj - "<<i+1<<" : ";
-        cin>>arr[i].weight;
-        cout<<"Enter Profit of Obj - "<<i+1<<" : ";
-        cin>>arr[i].value;
+    cout<<"Enter profits of each object"<<endl;
+    for(i=0; i<n; i++){
+        cin>>val;
+        p.push_back(val);
+    }
+    cout<<"Enter weights of each object"<<endl;
+    for(i=0; i<n; i++){
+        cin>>val;
+        w.push_back(val);
     }
 
-	cout << "Maximum possible profit = "
-		<< knapsack(W, arr, n);
+    cout<<"\nObject\tProfit\tWeight\tProfit/Weight Ratio"<<endl;
+    for(i=0; i<n; i++){
+        cout<<i+1<<"\t"<<p[i]<<"\t"<<w[i]<<"\t"<<p[i]/w[i]<<endl;
+        objList.push_back({double(p[i]/w[i]), p[i], w[i], double(i+1)});
+    }
 
-	return 0;
+    sort(objList.rbegin(), objList.rend());
+
+    // for(i=0; i<n; i++){
+    //     for(j=0; j<n; j++){
+    //         if(p[i]/w[i] > p[j]/w[j]){
+    //             temp=p[j];
+    //             p[j]=p[i];
+    //             p[i]=temp;
+    //             temp=w[j];
+    //             w[j]=w[i];
+    //             w[i]=temp;
+    //         }
+    //     }
+    // }
+
+    cout<<"\nAfter Sorting..."<<endl;
+    cout<<"\nObject\tProfit\tWeight\tRatio"<<endl;
+    for(i=0; i<n; i++){
+        //cout<<i+1<<"\t"<<p[i]<<"\t"<<w[i]<<endl;
+        cout<<objList[i][3]<<"\t"<<objList[i][1]<<"\t"<<objList[i][2]<<"\t"<<objList[i][0]<<endl;
+    }
+
+    Knapsack();
+    return 0;
 }
